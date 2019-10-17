0c535f8af4ca67fd1ffa982b019f45ee88e71cb1
Pandos <lobeyko1994@tut.by>
2019-10-17 12:08:38 +0300

    Add Project

---
+++ Arkanoid/Assets/Scripts/Ball_move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball_move : MonoBehaviour
+{
+    Rigidbody2D rig;
+    public float power_ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.AddForce(new Vector3(0.5f, 1.2f) * Time.deltaTime * power_ball);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.y <= -6.0f)
+        {
+            //Time.timeScale = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Arkanoid/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float playerSpeed;   
+    private Vector3 playerPosition;
+    public float boundary;
+
+    private int playerLives;
+    private int playerPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    // получим начальную позицию платформы
+    playerPosition = gameObject.transform.position;
+
+        playerLives = 3;
+        playerPoints = 0;
+    }
+
+    void addPoints(int points)
+    {
+        playerPoints += points;
+    }
+
+    void TakeLife()
+    {
+        playerLives--;
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(5.0f, 3.0f, 200.0f, 200.0f), "Live's: " + playerLives + "  Score: " + playerPoints);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // горизонтальное движение
+        playerPosition.x += Input.GetAxis("Mouse X") * playerSpeed * Time.deltaTime;
+
+        // выход из игры
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // обновим позицию платформы
+        transform.position = playerPosition;
+
+        // проверка выхода за границы
+        if (playerPosition.x < -boundary)
+        {
+            transform.position = new Vector3(-boundary, playerPosition.y, playerPosition.z);
+        }
+        if (playerPosition.x > boundary)
+        {
+            transform.position = new Vector3(boundary, playerPosition.y, playerPosition.z);
+        }
+
+        playerPosition = transform.position;
+    }
+}
