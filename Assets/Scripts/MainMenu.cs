9dd1b3ab86ad9bf32ba4763ff4ca35837c2ebe80
Pandos <lobeyko1994@tut.by>
2019-10-22 16:03:17 +0300

    pass between levels

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,19 +5,55 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
+    Scene scene;
     void OnMouseUpAsButton()
     {
         switch(gameObject.name)
         {
-            case "play_button": SceneManager.LoadScene("Level1");break;
-            case "ranking_button": Debug.Log("Ranking");break;
-            case "shop_button": Debug.Log("Shop"); break;
-            case "set_button": Debug.Log("Settings"); break;
-            case "exit_button": Debug.Log("Exit"); break;
-            case "return": SceneManager.LoadScene(SceneManager.GetActiveScene().name); break;
-            case "home": SceneManager.LoadScene("MainMenu"); break;
-            case "check": Debug.Log("Next Level"); break;
+            case "play_button":
+                {
+                    SceneManager.LoadScene(1); break;
+                }
+
+            case "ranking_button":
+                {
+                    Debug.Log("Ranking"); break;
+                }
+
+            case "shop_button":
+                {
+                    Debug.Log("Shop"); break;
+                }
+
+            case "set_button":
+                {
+                    Debug.Log("Settings"); break;
+                }
+
+            case "exit_button":
+                {
+                    Debug.Log("Exit"); break;
+                }
+            case "return":
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name); break;
+                }
+            case "home":
+                {
+                    SceneManager.LoadScene("MainMenu"); break;
+                }
+            case "check":
+                {
+                    scene = SceneManager.GetActiveScene();
+                    int sceneIndex = scene.buildIndex;
+                    sceneIndex++;
+                    if(sceneIndex > 6)
+                    {
+                        sceneIndex = 0;
+                    }
+                    SceneManager.LoadScene(sceneIndex);
+                }
+                break;
         }
     }
 }
